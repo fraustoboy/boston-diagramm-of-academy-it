dd93235af8426c706056a8fdf6b93b4a6e2d8169
fraustoboy <zhasminnaa@gmail.com>
2025-10-30 10:27:54 +0300

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,11 @@
 // next.config.ts
 import type { NextConfig } from "next";
 
-const repo = "YOUR_REPO_NAME"; // <— имя вашего репозитория
+const repo = "boston-diagramm-of-academy-it";
 
 const isProd = process.env.NODE_ENV === "production";
 const config: NextConfig = {
   output: "export",
-  // НУЖНО для Project Pages: https://username.github.io/<repo>/
   basePath: isProd ? `/${repo}` : undefined,
   assetPrefix: isProd ? `/${repo}/` : undefined,
 };
