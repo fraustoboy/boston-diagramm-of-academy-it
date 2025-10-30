3211a760ac71664e75690aebc4bd7cdce97ff70e
fraustoboy <zhasminnaa@gmail.com>
2025-10-30 10:56:02 +0300

    Delete src directory

--- src/components/ui/input.tsx
+++
@@ -1,4 +0,0 @@
-import * as React from "react";
-export function Input(props: React.InputHTMLAttributes<HTMLInputElement>) {
-  return <input {...props} className={`border rounded-lg px-3 py-2 w-full ${props.className || ""}`} />;
-}
